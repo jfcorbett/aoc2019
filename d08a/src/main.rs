8b91a08f4c49eb91d8c2984cc78c41ed0a1df17d
JEACO <jeaco@orsted.dk>
2019-12-11 21:44:07 +0100

    Finish d08a

--- d08a/src/main.rs
+++ d08a/src/main.rs
@@ -19,5 +19,26 @@ fn read_whole_file(s: impl AsRef<Path>) -> Result<String> {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let whole_file = read_whole_file(Path::new("input")).unwrap(); // String
+    let nums:Vec<_> = whole_file.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let width: usize = 25;
+    let height: usize = 6;
+    let n_pixels = width * height;
+    let n_layers = nums.len() / n_pixels;
+
+    // let xx = (0..n_layers)
+    //     .map(|l| nums[l*n_pixels..(l+1)*n_pixels].iter().filter(|&n| *n == 0_u8).count());
+    let xx = (0..n_layers)
+        .map(|l| &nums[l*n_pixels..(l+1)*n_pixels])
+        .map(|l| (0..=2).map(|d| l.iter().filter(|&n| *n == d).count()).collect::<Vec<_>>())
+        .min_by(|a,b| a[0].cmp(&b[0])).unwrap();
+    
+    
+    // for l in 0..n_layers {
+    //     let layer = &nums[l*n_pixels..(l+1)*n_pixels];
+
+    // }
+
+
+    println!("{:?}", xx[1]*xx[2]);
 }
