1179657024c100b2f02fac1f977246b34329da01
JEACO <jeaco@orsted.dk>
2019-12-14 20:34:49 +0100

    d12b cigar

--- d12b/fkn_lcm.py
+++ d12b/fkn_lcm.py
@@ -2,6 +2,8 @@ from math import gcd
 a=[18,28,44]
 a=[2028,5898,9404]
 a = [286332, 322856, 384944]   #will work for an int array of any length
+a=[286332,161428,1468]
+a=[286332,161428,96236]
 lcm = a[0]
 for i in a[1:]:
   lcm = int(lcm*i/gcd(lcm, i))
--- d12b/src/main.rs
+++ d12b/src/main.rs
@@ -16,38 +16,44 @@ fn read_whole_file(s: impl AsRef<Path>) -> Result<String> {
 }
 
 fn main() {
-    // let sgrofnak = read_whole_file(Path::new("input")).unwrap();
-    // let sgrofnak = sgrofnak
-    //     .replace("<x=", "")
-    //     .replace(" y=", "")
-    //     .replace(" z=", "")
-    //     .replace(">", "");
-    // let mut pos: Vec<Vec<i32>> = sgrofnak
-    //     .lines()
-    //     .map(|l| l.split(",").map(|n| n.parse::<i32>().unwrap()).collect())
-    //     .collect();
+    let sgrofnak = read_whole_file(Path::new("input")).unwrap();
+    let sgrofnak = sgrofnak
+        .replace("<x=", "")
+        .replace(" y=", "")
+        .replace(" z=", "")
+        .replace(">", "");
+    let mut pos: Vec<Vec<i32>> = sgrofnak
+        .lines()
+        .map(|l| l.split(",").map(|n| n.parse::<i32>().unwrap()).collect())
+        .collect();
     let mut vlc: Vec<Vec<i32>> = vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0]];
-    let mut pos: Vec<Vec<i32>> = vec![vec![0, 4, 0], vec![-10, -6, -14], vec![9, -16, -3], vec![6, -1, 2]];
+    // let mut pos: Vec<Vec<i32>> = vec![vec![0, 4, 0], vec![-10, -6, -14], vec![9, -16, -3], vec![6, -1, 2]];
     // let mut pos: Vec<Vec<i32>> = vec![vec![-1, 0, 2], vec![2, -10, -7], vec![4, -8, 8], vec![3, 5, -1]];
     // let mut pos: Vec<Vec<i32>> = vec![vec![-8, -10, 0], vec![5, 5, 10], vec![2, -7, 3], vec![9, -8, -3]];
 
     let pos_orig = pos.clone();
     let vlc_orig = vlc.clone();
-    let mut n_steps = 0_i32;
     let mut rs:Vec<i32> = Vec::new();
     for dim in 0..3 {
+        let mut n_steps = 0_i32;
+        let mut pos = pos_orig.clone();
+        let mut vlc = vlc_orig.clone();
         loop {
             n_steps += 1;
             moondance(&mut pos, &mut vlc);
-            // println!("{}: {:?}", n_steps, pos);
+            if n_steps == 1468 {
+                println!("{:?}", pos);
+            }
             if pos.iter().map(|p| p[dim]).collect::<Vec<_>>() == pos_orig.iter().map(|p| p[dim]).collect::<Vec<_>>() 
-                // && vlc.iter().map(|p| p[dim]).collect::<Vec<_>>() == vlc_orig.iter().map(|p| p[dim]).collect::<Vec<_>>() 
+                && vlc.iter().map(|p| p[dim]).collect::<Vec<_>>() == vlc_orig.iter().map(|p| p[dim]).collect::<Vec<_>>() 
                 {
                 break
             }
         }
         rs.push(n_steps);
         println!("{:?}", n_steps);
+        // println!("{:?}", pos);
+        // println!("{:?}", vlc);
         // println!("{}: {:?}", n_steps, pos);
     }
     // Calculate Lowest Common Multiple of the three numbers of steps for x,y,z dims....
