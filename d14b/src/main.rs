3805b933ee07f15e97cf0af4f2ddd073aa57ca49
JEACO <jeaco@orsted.dk>
2019-12-17 09:53:37 +0100

    Smash d14b

--- d14b/src/main.rs
+++ d14b/src/main.rs
@@ -27,19 +27,38 @@ fn main() {
     let wrokfluge = read_whole_file(Path::new("input")).unwrap();
     let reac_specs = parse_reac_specs(&wrokfluge);
     
-    let mut leftovers = HashMap::new();
-    println!("{}", ore_needed("FUEL", 1, &reac_specs, &mut leftovers));
+    println!("{}", max_num_produceable("FUEL", ORE_RESERVES, &reac_specs));
+
 }
 
-fn max_num_produceable(chem: &str, num_ore: u64, reac_specs: &HashMap<String, (u64, Vec<(u64, String)>)>) -> u64 {
-    let lb = ORE_RESERVES / ore_needed(chem, 1, &reac_specs, &mut HashMap::new());
-    let mut num_chem_prev: u64 = lb;
+fn max_num_produceable(chem: &str, max_ore: u64, reac_specs: &HashMap<String, (u64, Vec<(u64, String)>)>) -> u64 {
+    let mut lb = max_ore / ore_needed(chem, 1, &reac_specs, &mut HashMap::new());
+    let mut ub = 2*lb;
+    // Find an upper bound
     loop {
-        let try_num_chem = num_chem_prev + 1;
-        if ore_needed(chem, try_num_chem, &reac_specs, &mut HashMap::new()) > ORE_RESERVES {
-            return num_chem_prev;
+        println!("{} {}", lb, ub);
+        if ore_needed(chem, ub, &reac_specs, &mut HashMap::new()) > max_ore {
+            break;
+        } else {
+            ub *= 2;
         };
-        num_chem_prev = try_num_chem;
+    }
+    // Bisection
+    loop {
+        println!("{} {}", lb, ub);
+        if ub - lb == 1 {
+            if ore_needed(chem, ub, &reac_specs, &mut HashMap::new()) > max_ore {
+                return lb;
+            } else {
+                return ub;
+            }
+        }
+        let try_num_chem = (ub+lb)/2;
+        if ore_needed(chem, try_num_chem, &reac_specs, &mut HashMap::new()) > max_ore {
+            ub = try_num_chem;
+        } else {
+            lb = try_num_chem;
+        };
     }
 }
 
@@ -73,6 +92,14 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_max_num_produceable_trivial() {
+        let reac_specs = parse_reac_specs("91 ORE => 20 FUEL");
+        assert_eq!(max_num_produceable("FUEL", 91, &reac_specs), 20);
+        assert_eq!(max_num_produceable("FUEL", 181, &reac_specs), 20);
+        assert_eq!(max_num_produceable("FUEL", 182, &reac_specs), 40);
+    }
+
+    #[test]
     fn test_max_num_produceable_example2() {
         let reac_specs = parse_reac_specs("157 ORE => 5 NZVS
 165 ORE => 6 DCFZ
@@ -84,6 +111,45 @@ mod tests {
 165 ORE => 2 GPVTF
 3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT");        
         assert_eq!(max_num_produceable("FUEL", ORE_RESERVES, &reac_specs), 82892753);
+    }
+
+    #[test]
+    fn test_max_num_produceable_example3() {
+        let reac_specs = parse_reac_specs("2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF");        
+        assert_eq!(max_num_produceable("FUEL", ORE_RESERVES, &reac_specs), 5586022);
+    }
+
+    #[test]
+    fn test_max_num_produceable_example4() {
+        let reac_specs = parse_reac_specs("171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX");        
+        assert_eq!(max_num_produceable("FUEL", ORE_RESERVES, &reac_specs), 460664);
     }
 
     #[test]
